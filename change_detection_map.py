a379ea4a5dc10a1e623a0f1525d86e82537df064
willjayeo <willjay8@hotmail.com>
2025-09-21 17:26:33 +0100

    Moved normalising to function

--- change_detection_map.py
+++ change_detection_map.py
@@ -23,6 +23,42 @@ def setup_logger(verbose: bool = False, debug: bool = False):
         logging.setLevel(logging.WARNING)
 
 
+def normalise_arrays(
+    img_a_array: xarray.DataArray, img_b_array: xarray.DataArray
+) -> tuple:
+    """
+    Return arrays containing data normalised to the maximum range of the combined
+    input arrays
+    """
+
+    # Get data value ranges
+    min_a = img_a_array.min()
+    max_a = img_a_array.max()
+    min_b = img_b_array.min()
+    max_b = img_b_array.max()
+
+    # Get the lowest value of the combined arrays
+    if max_a > max_b:
+        range_max = max_a
+    else:
+        range_max = max_b
+
+    # Get the maximum value of the combined arrays
+    if min_a < min_b:
+        range_min = min_a
+    else:
+        range_min = min_b
+
+    # Calculate the total data range of the two arrays
+    data_range = range_max - range_min
+
+    # Normalise data
+    img_a_array_normalised = (img_a_array - range_min) / data_range
+    img_b_array_normalised = (img_b_array - range_min) / data_range
+
+    return img_a_array_normalised, img_b_array_normalised
+
+
 def main(img_a_path: str, img_b_path: str, verbose=False, debug=False):
     """ """
 
@@ -35,30 +71,9 @@ def main(img_a_path: str, img_b_path: str, verbose=False, debug=False):
     img_a_array = img_a_dataset.to_dataarray()
     img_b_array = img_b_dataset.to_dataarray()
 
-    # Get data value ranges
-    min_a = img_a_array.min()
-    max_a = img_a_array.max()
-    min_b = img_b_array.min()
-    max_b = img_b_array.max()
+    # Normalise array values by the combined range of values
+    img_a_array_normalised, img_b_array_normalised = normalise_arrays(img_a_array, img_b_array)
 
-    range_a = max_a - min_a
-    range_b = max_b - min_b
-
-    if max_a > max_b:
-        range_max = max_a
-    else:
-        range_min = max_b
-
-    if min_a < min_b:
-        range_min = min_a
-    else:
-        range_min = min_b
-
-    data_range = range_max - range_min
-
-    # Normalise data
-    img_a_array_normalised = (img_a_array - range_min) / data_range
-    img_b_array_normalised = (img_b_array - range_min) / data_range
 
     # TODO: Get a good contrast?
 
